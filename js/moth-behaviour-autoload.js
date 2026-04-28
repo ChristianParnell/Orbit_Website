04aa22b54204cf37e9e0e6b7b3b7792942882e2e
ChristianParnell <81597198+ChristianParnell@users.noreply.github.com>
2026-04-28 16:02:52 +1200

    Delete js/moth-behaviour-autoload.js

--- js/moth-behaviour-autoload.js
+++
@@ -1,9 +0,0 @@
-// Orbit Website — Moth Behaviour Stabilizer Autoload
-// Drop into: js/moth-behaviour-autoload.js
-// Then load this module BEFORE ./js/main.js in index.html.
-
-import * as THREE from "https://esm.sh/three@0.160.0";
-import { MothSystem } from "./moth-system.js";
-import { installMothBehaviourStabilizer } from "./moth-behaviour-stabilizer.js";
-
-installMothBehaviourStabilizer(MothSystem, THREE);
